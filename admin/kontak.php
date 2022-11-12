d32f1281df04c75c05ade7332c1fd13e22639035
Fadilah Riczky <friczky@gmail.com>
2022-11-12 17:30:11 +0700

    :zap: update admin kontak

--- admin/kontak.php
+++ admin/kontak.php
@@ -1,13 +1,44 @@
+<?php
+if (isset($_POST['update'])){
+	$id = 1;
+	$email = $_POST['email'];
+	$telpon = $_POST['telpon'];
+	$waktu = $_POST['waktu'];
+	$maps = $_POST['maps'];
+	$logo = $_FILES['logo']['name'];
+	$logo_old   = $_POST['logo_old'];
+	$tmp = $_FILES['logo']['tmp_name'];
+	$logo_save = rand().$_FILES['logo']['name'];
+	if ($logo != $logo_old) {
+		unlink('uploads/'.$logo_old);
+		move_uploaded_file($tmp, 'uploads/'.$logo_save);
+	}
+	$sql = "UPDATE kontak SET email = '$email',telpon = '$telpon',waktu = '$waktu', maps = '$maps',logo = '$logo_save' WHERE id='1'";
+	$query = mysqli_query($koneksi,$sql);
+	if($query){
+		echo '<script>alert("Data Berhasil Diubah");window.location.href="index.php?halaman=kontak"</script>';
+	}else{
+		echo 'Gagal';
+	}
+} else {
+	$sql = "SELECT * FROM kontak WHERE id='1'";
+	$result = $koneksi->query($sql);
+	foreach ($result as $kontak) {
+
+}
+
+?>
 
 <h2>KONTAK</h2>
 <hr>
+<form action="" method="post" enctype="multipart/form-data">
 <div class="form-group">
 	<div class="row">
 		<div class="col-sm-1">
 		<label for="">Email</label>
 		</div>
 		<div class="col-sm-11">
-		<input type="text" class="form-control" name="nama_toko" value="">
+		<input type="text" class="form-control" name="email" value="<?= $kontak['email']?>">
 		</div>
 	</div>
 </div>
@@ -18,7 +49,18 @@
 		<label for="">Whatsapp</label>
 		</div>
 		<div class="col-sm-11">
-		<input type="text" class="form-control" name="nama_toko" value="">
+		<input type="text" class="form-control" name="telpon" value="<?= $kontak['telpon']?>">
+		</div>
+	</div>
+</div>
+
+<div class="form-group">
+	<div class="row">
+		<div class="col-sm-1">
+		<label for="">Waktu Buka</label>
+		</div>
+		<div class="col-sm-11">
+		<input type="text" class="form-control" name="waktu" value="<?= $kontak['waktu']?>">
 		</div>
 	</div>
 </div>
@@ -29,7 +71,7 @@
 		<label for="">Maps Url</label>
 		</div>
 		<div class="col-sm-11">
-		<textarea class="form-control" name="" id="" cols="10" rows="10"></textarea>
+		<textarea class="form-control" name="maps" id="" cols="10" rows="5"><?= $kontak['maps']?></textarea>
 		</div>
 	</div>
 </div>
@@ -40,7 +82,8 @@
 		<label for="">Logo</label>
 		</div>
 		<div class="col-sm-11">
-		<input type="file" class="form-control" name="foto" value="">
+		<input type="file" class="form-control" name="logo" value="">
+		<input type="hidden" class="form-control" name="logo_old" value="<?= $kontak['logo']?>">
 		</div>
 	</div>
 </div>
@@ -51,7 +94,18 @@
 		<label for="">Aksi</label>
 		</div>
 		<div class="col-sm-11">
-		<button class="btn btn-primary">Simpan</button>
+		<button type="submit" name="update" class="btn btn-primary">Simpan</button>
 		</div>
 	</div>
 </div>
+</form>
+<?php }?>
+
+<?php
+
+
+
+
+
+
+?>
--- admin/tentang.php
+++ admin/tentang.php
@@ -1,13 +1,41 @@
+<?php
+if (isset($_POST['update'])){
+	$id = 1;
+	$nama = $_POST['nama_toko'];
+	$deskripsi = $_POST['deskripsi'];
+	$logo = $_FILES['logo']['name'];
+	$logo_old   = $_POST['logo_old'];
+	$tmp = $_FILES['logo']['tmp_name'];
+	$logo_save = rand().$_FILES['logo']['name'];
+	if ($logo != $logo_old) {
+		unlink('uploads/'.$logo_old);
+		move_uploaded_file($tmp, 'uploads/'.$logo_save);
+	}
+	$sql = "UPDATE tentang SET nama_toko = '$nama',deskripsi = '$deskripsi',logo = '$logo_save' WHERE id='1'";
+	$query = mysqli_query($koneksi,$sql);
+	if($query){
+		echo '<script>alert("Data Berhasil Diubah");window.location.href="index.php?halaman=tentang"</script>';
+	}else{
+		echo 'Gagal';
+	}
+} else {
+	$sql = "SELECT * FROM tentang WHERE id='1'";
+	$result = $koneksi->query($sql);
+	foreach ($result as $tentang) {
 
-<h2>TENTANG</h2>
+}
+?>
+<h2>TENTANG <small><?= $tentang['nama_toko']?></small></h2> 
 <hr>
+
+<form action="" method="post" enctype="multipart/form-data">
 <div class="form-group">
 	<div class="row">
 		<div class="col-sm-1">
 		<label for="">Nama Toko</label>
 		</div>
 		<div class="col-sm-11">
-		<input type="text" class="form-control" name="nama_toko" value="">
+		<input type="text" class="form-control" name="nama_toko" value="<?= $tentang['nama_toko']?>">
 		</div>
 	</div>
 </div>
@@ -18,7 +46,7 @@
 		<label for="">Deskripsi</label>
 		</div>
 		<div class="col-sm-11">
-		<textarea class="form-control" name="" id="" cols="10" rows="10"></textarea>
+		<textarea class="form-control" name="deskripsi" id="" cols="10" rows="10"><?= $tentang['deskripsi']?></textarea>
 		</div>
 	</div>
 </div>
@@ -29,7 +57,8 @@
 		<label for="">Foto</label>
 		</div>
 		<div class="col-sm-11">
-		<input type="file" class="form-control" name="foto" value="">
+		<input type="file" class="form-control" name="logo" value="">
+		<input type="hidden" class="form-control" name="logo_old" value="<?= $tentang['foto']?>">
 		</div>
 	</div>
 </div>
@@ -40,7 +69,9 @@
 		<label for="">Aksi</label>
 		</div>
 		<div class="col-sm-11">
-		<button class="btn btn-primary">Simpan</button>
+		<button type="submit" name="update" class="btn btn-primary">Simpan</button>
 		</div>
 	</div>
 </div>
+</form>
+<?php }?>
